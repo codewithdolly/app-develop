7ddebb9f26a9bf07ee256328d4cbe3d40b8c869d
hicoderindia <hicoder.dolly@gmail.com>
2022-02-11 00:04:02 +0530

    update

--- src/Components/HotBets/HotBets.js
+++ src/Components/HotBets/HotBets.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import "./HotBets.css";
 import { useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
@@ -16,27 +17,61 @@ const HotBets = () => {
   const theme = useTheme();
 
   return (
-    <Card sx={{ display: "flex", maxWidth: 245 }}>
-      <Avatar alt="Remy Sharp" src={img1} sx={{ width: 50, height: 50 }} />
-      <Box sx={{ display: "flex", flexDirection: "column" }}>
-        <CardContent sx={{ flex: "1 0 auto" }}>
-          <Typography component="div" variant="h6" sx={{ color: "black" }}>
-            Luck Marlow
-          </Typography>
-          <div style={{ display: "flex" }}>
-            <Avatar src={icon1} variant="rounded" />
-            <Typography variant="subtitle1" component="div">
-              108% PROFIT
+      <div className="hotBets">   
+      {betCards.map((card)=>{
+          return (<>
+            <Card sx={{ maxWidth: 250, p: 1 }}>
+      <div style={{ display: "flex" }}>
+        <Avatar alt="Remy Sharp" src={img1} sx={{ width: 50, height: 50 }} />
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <CardContent sx={{ flex: "1 0 auto" }}>
+            <Typography component="div" variant="h6" sx={{ color: "black" }}>
+              Luck Marlow
             </Typography>
-          </div>
-        </CardContent>
-        <div style={{ display: "flex", justifyContent: "spaceBetween" }}>
-          <Button href="#text-buttons">Link</Button>
-          <Button variant="contained">Contained</Button>
-        </div>
-      </Box>
+            <div style={{ display: "flex" }}>
+              <Avatar src={icon1} variant="rounded" />
+              <Typography variant="subtitle1" component="div">
+                108% PROFIT
+              </Typography>
+            </div>
+          </CardContent>
+        </Box>
+      </div>
+      <div style={{ display: "flex", justifyContent: "space-around" }}>
+        <Button href="#text-buttons" className="btn" color="error">
+          1m25s
+        </Button>
+        <Button variant="contained" className="btn btn2">
+          HOT Bet Now
+        </Button>
+      </div>
     </Card>
+          </>)
+      })}
+    </div>
+
   );
 };
 
 export default HotBets;
+
+const betCards=[
+    {
+        name:"Luck Marlow",
+    },
+    {
+        name:"Luck Marlow",
+    },
+    {
+        name:"Luck Marlow",
+    },
+    {
+        name:"Luck Marlow",
+    },
+    {
+        name:"Luck Marlow",
+    },
+    {
+        name:"Luck Marlow",
+    },
+]
